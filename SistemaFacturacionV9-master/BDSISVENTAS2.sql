5b7b38523e3a40b079d20d95a700f56813cbb991
Julissa <julissadiaz849@gmail.com>
2023-11-10 21:49:03 -0600

    Agregar archivos de proyecto.

---
+++ SistemaFacturacionV9-master/BDSISVENTAS2.sql
@@ -0,0 +1,216 @@
+ CREATE DATABASE DBSISTEMA_VENTA
+
+GO
+
+USE DBSISTEMA_VENTA
+
+go
+
+CREATE TABLE ROL(
+IdRol int primary key identity,
+Descripcion varchar(150),
+FechaRegistro datetime default getdate ()
+);
+go
+
+CREATE TABLE PERMISO(
+IdPermiso int primary key identity,
+IdRol int references ROL(IdRol),
+NombreMenu varchar(150),
+FechaRegistro datetime default getdate ()
+);
+go
+
+CREATE TABLE PROVEEDOR(
+IdProveedor int primary key identity,
+Documento varchar(150),
+RazonSocial varchar(150),
+Correo varchar(150),
+Telefono varchar(150),
+Estado bit,
+FechaRegistro datetime default getdate ()
+);
+
+go
+
+CREATE TABLE CLIENTE(
+IdCliente int primary key identity,
+Documento varchar(150),
+NombreCompleto varchar(150),
+Correo varchar(150),
+Telefono varchar(150),
+Estado bit,
+FechaRegistro datetime default getdate ()
+);
+
+go
+
+CREATE TABLE USUARIO(
+IdUsuario int primary key identity,
+Documento varchar(150),
+NombreCompleto varchar(150),
+Correo varchar(150),
+Clave varchar(150),
+IdRol int references ROL(IdRol),
+Estado bit,
+FechaRegistro datetime default getdate ()
+);
+
+go
+
+CREATE TABLE CATEGORIA(
+IdCategoria int primary key identity,
+Descripcion varchar(100),
+Estado bit,
+FechaRegistro datetime default getdate ()
+);
+
+go
+
+CREATE TABLE PRODUCTO(
+IdProducto int primary key identity,
+Codigo varchar(150),
+Nombre varchar (150),
+Descripcion varchar(150),
+IdCategoria int references CATEGORIA(IdCategoria),
+Stock int not null default 0,
+PrecioCompra decimal (10,2) default 0,
+PrecioVenta decimal (10,2) default 0,
+Estado bit, 
+FechaRegistro datetime default getdate ()
+);
+
+ 
+go
+
+CREATE TABLE COMPRA(
+IdCompra int primary key identity,
+IdUsuario int references USUARIO (IdUsuario),
+IdProveedor int references PROVEEDOR (IdProveedor),
+TipoDocumento varchar(150),
+NumeroDocumento varchar(150),
+MontoTotal int references ROL(IdRol),
+FechaRegistro datetime default getdate ()
+);
+
+go
+
+
+CREATE TABLE DETALLE_COMPRA(
+IdDetalleCompra int primary key identity,
+IdCompra int references COMPRA (IdCompra),
+IdProducto int references PRODUCTO (IdProducto),
+PrecioCompra decimal (10,2) default 0,
+PrecioVenta decimal (10,2) default 0,
+Cantidad int,
+MontoTotal decimal (10,2),
+FechaRegistro datetime default getdate ()
+);
+
+go
+
+CREATE TABLE VENTA (
+IdVenta int primary key identity,
+IdUsuario int references USUARIO(IdUsuario),
+TipoDocumento varchar (150),
+NumeroDocumento varchar (150),
+DocumentoCliente varchar (150),
+NombreCliente varchar (150),
+MontoPago decimal (10,2),
+MontoCambio decimal (10,2),
+MontoTotal decimal (10,2),
+FechaRegistro datetime default getdate ()
+);
+
+go
+
+CREATE TABLE DETALLE_VENTA(
+IdDetalleVenta int primary key identity,
+IdVenta int references VENTA(IdVenta),
+IdProducto int references PRODUCTO(IdProducto),
+PrecioVenta decimal (10,2),
+Cantidad int,
+SubTotal decimal (10,2),
+FechaRegistro datetime default getdate ()
+);
+go
+
+CREATE TABLE NEGOCIO(
+IdNegocio int primary key,
+Nombre varchar(150),
+RUC varchar (150),
+Direccion varchar (150),
+Logo varbinary (MAX) NULL
+);
+
+
+-------------------Registros
+
+go
+insert into ROL(Descripcion)
+values('Empleado');
+
+go 
+insert into PERMISO(IdRol, NombreMenu) values
+(2, 'menuusuarios'),
+(2, 'menucompras'),
+(2, 'menuclientes'),
+(2, 'menuproveedores'),
+(2, 'menuacercade')
+;
+go
+insert into ROL(Descripcion)
+values('Administrador');
+go
+insert into PERMISO(IdRol, NombreMenu) values
+(1, 'menuusuarios'),
+(1, 'menumantenedor'),
+(1, 'menuventas'),
+(1, 'menucompras'),
+(1, 'menuclientes'),
+(1, 'menuproveedores'),
+(1, 'menureportes'),
+(1, 'menuacercade')
+ 
+go
+insert into usuario(Documento, NombreCompleto, Correo, Clave, IdRol, Estado)
+values('juli22','Julissa D az','julissadiaz849@gmail.com', '12345', 1,1);
+go
+insert into usuario(Documento, NombreCompleto, Correo, Clave, IdRol, Estado)
+values('sebas22','Alejandro L e','alejandro@gmail.com', '67890', 1,1);
+go
+insert into PRODUCTO (Codigo, Nombre, Descripcion, IdCategoria, Estado) values
+('10110', 'gaseosa', '1litro', 2, 1)
+go
+insert into NEGOCIO (IdNegocio, Nombre, RUC, Direccion)
+Values
+(1,'Tieda Morelia','101010', 'Ahuachapan'); 
+
+select * from NEGOCIO
+
+select p.IdRol, NombreMenu from PERMISO p
+inner join ROL r on r.IdRol = p.IdRol
+inner join USUARIO  u on u.IdRol = r.IdRol
+where u.IdUsuario= 2;
+
+select p.IdRol, NombreMenu from PERMISO p
+inner join ROL r on r.IdRol = p.IdRol
+inner join USUARIO  u on u.IdRol = r.IdRol
+where u.IdUsuario= 3;
+
+select u.IdUsuario, u.Documento, u.NombreCompleto, u.correo, u.Clave, u.Estado, r.IdRol, r.Descripcion from usuario u inner join rol r on r.IdRol = u.IdRol
+
+select IdProducto, Codigo, Nombre, p.Descripcion, c.IdCategoria, c.Descripcion[DescripcionCategoria], Stock, PrecioCompra, PrecioVenta, p.Estado from PRODUCTO p
+inner join CATEGORIA c on c.IdCategoria = p.IdCategoria
+
+select*from NEGOCIO
+
+select*from USUARIO
+
+select * from COMPRA
+select * from USUARIO
+select * from PROVEEDOR
+
+INSERT INTO COMPRA (IdUsuario, IdProveedor, TipoDocumento, NumeroDocumento,MontoTotal, FechaRegistro)
+values
+(1,1,'12314','293113',2,'2023-11-9' );
---
+++ SistemaFacturacionV9-master/CapaDatos/CD_Venta.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+
+namespace CapaDatos
+{
+    public class CD_Venta
+    {
+        public int ObtenerCorrelativo()
+        {
+            int idCorrelativo = 0;
+
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("select count(*) + 1 from VENTA");
+
+
+                    SqlCommand cmd = new SqlCommand(query.ToString(), oconexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    idCorrelativo = Convert.ToInt32(cmd.ExecuteScalar());
+
+
+                }
+                catch (Exception ex)
+                {
+                    idCorrelativo = 0;
+                }
+            }
+
+            return idCorrelativo;
+        }
+
+        public bool RestarStock(int idproducto, int cantidad)
+        {
+            bool Respuesta = true;
+
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("update producto set stock= stock - @cantidad where idproducto= @idproducto");
+                    SqlCommand cmd = new SqlCommand(query.ToString(), oconexion);
+                    cmd.Parameters.AddWithValue("@cantidad", cantidad);
+                    cmd.Parameters.AddWithValue("@idproducto", idproducto);
+                    cmd.CommandType= CommandType.Text;
+                    oconexion.Open();
+
+
+                    Respuesta = cmd.ExecuteNonQuery() > 0 ? true : false;
+
+
+                }
+                catch (Exception ex)
+                {
+                    Respuesta = false;
+                }
+            }
+
+            return Respuesta;
+        }
+
+        public bool SumarStock(int idproducto, int cantidad)
+        {
+            bool Respuesta = true;
+
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("update producto set stock= stock + @cantidad where idproducto= @idproducto");
+                    SqlCommand cmd = new SqlCommand(query.ToString(), oconexion);
+                    cmd.Parameters.AddWithValue("@cantidad", cantidad);
+                    cmd.Parameters.AddWithValue("@idproducto", idproducto);
+                    cmd.CommandType = CommandType.Text;
+                    oconexion.Open();
+
+
+                    Respuesta = cmd.ExecuteNonQuery() > 0 ? true : false;
+
+
+                }
+                catch (Exception ex)
+                {
+                    Respuesta = false;
+                }
+            }
+
+            return Respuesta;
+        }
+
+        public bool Registrar(Venta obj, DataTable DetalleVenta, out string Mensaje)
+        {
+            bool Respuesta = false;
+            Mensaje = string.Empty;
+
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+
+
+                try
+                {
+
+                    SqlCommand cmd = new SqlCommand("usp_RegistrarVenta", oconexion);
+                    cmd.Parameters.AddWithValue("IdUsuario", obj.oUsuario.IdUsuario);
+                    cmd.Parameters.AddWithValue("TipoDocumento", obj.TipoDocumento);
+                    cmd.Parameters.AddWithValue("NumeroDocumento", obj.NumeroDocumento);
+                    cmd.Parameters.AddWithValue("DocumentoCliente", obj.DocumentoCliente);
+                    cmd.Parameters.AddWithValue("NombreCliente", obj.NombreCliente);
+                    cmd.Parameters.AddWithValue("MontoPago", obj.MontoPago);
+                    cmd.Parameters.AddWithValue("MontoCambio", obj.MontoCambio);
+                    cmd.Parameters.AddWithValue("MontoTotal", obj.MontoTotal);
+                    cmd.Parameters.AddWithValue("DetalleVenta", DetalleVenta);
+                   
+
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oconexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+
+
+
+                    Respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+
+                }
+                catch (Exception ex)
+                {
+                    Respuesta = false;
+                    Mensaje = ex.Message;
+                }
+            }
+            return Respuesta;
+        }
+
+
+    }
+}
